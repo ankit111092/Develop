c12a5b6b41ff8da8183c32bede60ad68be5b804d
ankitkumar <ankit.kumar3@soprasteria.com>
2020-04-08 19:21:21 +0530

    Upload ALl Project

---
+++ TravelWeb/TravelWeb/Controllers/CarrersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelWeb.Controllers
+{
+    public class CarrersController : Controller
+    {
+        public IActionResult Carrers()
+        {
+            return View();
+        }
+    }
+}
---
+++ TravelWeb/TravelWeb/Controllers/IndexController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelWeb.Controllers
+{
+    public class IndexController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult _Menu()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public IActionResult Careers()
+        {
+            return View();
+
+        }
+        [HttpGet]
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
